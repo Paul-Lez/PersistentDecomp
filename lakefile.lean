acdad0aca225a5da8a3d17daea8897a4133bddb7
Yaël Dillies <yael.dillies@gmail.com>
2024-11-28 11:09:57 +0000

    Get the website working

--- lakefile.lean
+++ lakefile.lean
@@ -10,6 +10,9 @@ package "PersistentDecomp" where
 
 require "leanprover-community" / "mathlib"
 
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib PersistentDecomp where
-  -- add any library configuration options here
